0bc1f58f5d576b4219d9eb8847fa6284338bc206
Reinier <reinier@bettyblocks.com>
2023-10-18 22:30:14 +0200

    mobile improvements

--- app/page.tsx
+++ app/page.tsx
@@ -166,7 +166,7 @@ export default function Home() {
               persoonlijk advies.
             </p>
           </div>
-          <div className="md:ml-8 pb-12 m-8">
+          <div className="flex md:ml-8 m-8 md:m-0 justify-center">
             <Image
               src="/Trap met verlichting en antraciete treden.png"
               alt=""
@@ -178,7 +178,7 @@ export default function Home() {
       </section>
       <section className="bg-white">
         <div className="grid md:grid-cols-2 grid-row-1 gap-4 md:h-[500px] pt-12 md:py-[84px] container mx-auto">
-          <div className="md:mr-8 p-8 order-2 pb-12">
+          <div className="flex md:mr-8 p-8 md:p-0 order-2 md:order-1 pb-12 justify-center">
             <Image
               src="/Trap met bruine treden.png"
               alt=""
@@ -186,7 +186,7 @@ export default function Home() {
               height="150"
             />
           </div>
-          <div className="md:ml-8 order-1">
+          <div className="md:ml-8 order-1 md:order-2">
             <h2 className="text-3xl font-extrabold text-black text-center">
               Treed binnen in onze showroom
             </h2>
@@ -264,18 +264,16 @@ export default function Home() {
       </section>
       <footer className="bg-gray-300">
         <div className="grid md:grid-cols-2 grid-row-1 gap-12 md:gap-4 md:h-[200px] py-12 md:py-[84px] container mx-auto">
-          <div className="md:mr-8 text-black order-2 md:text-left text-center">
+          <div className="md:mr-8 text-black order-2 md:order-1 md:text-left text-center">
             © 2023 Kompleet Traprenovatie
           </div>
-          <div className="md:ml-8">
-            <div className="space-x-8 text-black order-1 md:text-left text-center">
-              <button className="text-lg ">Gallery</button>
-              <button className="text-lg">Over ons</button>
-              <button className="text-lg">Contact</button>
-              <button className="inline-flex items-center justify-center px-4 py-2 text-base font-bold leading-6 text-white bg-red-700 border border-transparent rounded-full md:w-auto hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700">
-                Configureer trap
-              </button>
-            </div>
+          <div className="md:ml-8 space-x-8 text-black order-1 md:order-2 md:text-left text-center">
+            <button className="text-lg">Gallery</button>
+            <button className="text-lg">Over ons</button>
+            <button className="text-lg">Contact</button>
+            <button className="inline-flex items-center justify-center px-4 py-2 text-base font-bold leading-6 text-white bg-red-700 border border-transparent rounded-full md:w-auto hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-700">
+              Configureer trap
+            </button>
           </div>
         </div>
       </footer>
