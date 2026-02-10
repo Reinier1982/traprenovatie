625610769981300dff147f3f9d125eff05797eff
Reinier <reinier@bettyblocks.com>
2026-02-10 22:16:00 +0100

    update address

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -94,8 +94,8 @@ const contact = () => {
                 <p className="mb-4">
                   <b className="text-xl">Kompleet Trap-Renovatie</b>
                 </p>
-                <p>Julianaweg 200</p>
-                <p className="mb-4">1131 DL Volendam</p>
+                <p>Baardmosstraat 135</p>
+                <p className="mb-4">1441 LV Purmerend</p>
                 <p>Telefoonummer: 06 3970 2331</p>
                 <p>
                   E-mail:{" "}
